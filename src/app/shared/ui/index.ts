cac75847b05a252ba84e4da90e75537d90873c05
Andrew <117319334+andreytr449@users.noreply.github.com>
2026-02-25 20:44:35 +0200

    feat: create user details page and shareable question link

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -5,3 +5,5 @@ export * from "./dialog";
 export { Input } from "./input";
 export { Providers } from "./providers";
 export { ErrorText } from "./error-form-text";
+export { UserProfile } from "./user-profle";
+export { Toaster } from "./sonner";
---
+++ src/app/shared/ui/user-profle.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+export const UserProfile = ({
+  nickname,
+  totalQuestions,
+  completedQuestions,
+  createdAt,
+}: {
+  nickname: string;
+  createdAt: Date;
+  totalQuestions: number;
+  completedQuestions: number;
+}) => {
+  return (
+    <section className="flex text-white flex-col justify-center w-92 items-center bg-modal rounded-xl px-10 py-2">
+      <Image
+        src="/UserProfilePhoto.png"
+        width={200}
+        height={200}
+        alt="user profile photo"
+        className="rounded-full"
+      />
+      <p className="text-2xl font-black">{nickname}</p>
+      <div className="flex justify-between gap-5 items-center">
+        <p className="text-gray items-start">Total Questions:</p>
+        <p className="">{totalQuestions}</p>
+      </div>
+      <div className="flex justify-between gap-5 items-center">
+        <p className="text-gray items-start">Created at:</p>
+        <p className="">
+          {createdAt.toLocaleDateString("en-EN", {
+            day: "numeric",
+            month: "long",
+          })}
+        </p>
+      </div>
+      <div className="flex justify-between gap-5 items-center">
+        <p className="text-gray">Answered questions:</p>
+        <p className="">{completedQuestions}</p>
+      </div>
+    </section>
+  );
+};
